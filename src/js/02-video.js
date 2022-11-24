3ccd8e6d229c1ece503a87d36cb69b50f83fa542
YuliiaKon <u.pabat@gmail.com>
2022-11-24 22:10:42 +0200

    lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
 
+// <iframe
+//   id="vimeo-player"
+//   src="https://player.vimeo.com/video/236203659"
+//   width="640"
+//   height="360"
+//   frameborder="0"
+//   allowfullscreen
+//   allow="autoplay; encrypted-media"
+// ></iframe>
+    
+
+// <script src="https://player.vimeo.com/api/player.js"></script>;
+// <script>
+//     const iframe = document.querySelector('iframe');
+//     const player = new Vimeo.Player(iframe);
+
+//     player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+// </script>
