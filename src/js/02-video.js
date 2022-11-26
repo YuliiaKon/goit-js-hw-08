6921c93faac3995d46c0c67368b9752d75870166
YuliiaKon <u.pabat@gmail.com>
2022-11-26 22:29:05 +0200

    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,25 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
-// <iframe
-//   id="vimeo-player"
-//   src="https://player.vimeo.com/video/236203659"
-//   width="640"
-//   height="360"
-//   frameborder="0"
-//   allowfullscreen
-//   allow="autoplay; encrypted-media"
-// ></iframe>
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    // videoplayer-current-time
+
+    // const onPlay = function(data) {
+    // };
+
+//     player.on('play', onPlay);
     
-
-// <script src="https://player.vimeo.com/api/player.js"></script>;
-// <script>
-//     const iframe = document.querySelector('iframe');
-//     const player = new Vimeo.Player(iframe);
-
-//     player.on('play', function() {
-//         console.log('played the video!');
-//     });
-
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
-// </script>
+// videoplayer - current - time: timeupdate;
+localStorage.setItem('current-time', JSON.stringify({ currentTime }));
+console.log(localStorage);
